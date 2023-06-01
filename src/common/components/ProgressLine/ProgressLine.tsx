aeef6f7b847b9691d9e45ee96099e88869bb4cbc
ksasrg <ksahtmlcss@gmail.com>
2023-06-01 13:01:52 +0300

    Updates

--- src/common/components/ProgressLine/ProgressLine.tsx
+++
@@ -1,8 +0,0 @@
-import LinearProgress from "@mui/material/LinearProgress/LinearProgress";
-import { useAppSelector } from "app/hooks";
-
-export function ProgressLine() {
-  const isLoading = useAppSelector((state) => state.app.isLoading);
-
-  return <div style={{ height: "4px" }}>{isLoading && <LinearProgress />}</div>;
-}
